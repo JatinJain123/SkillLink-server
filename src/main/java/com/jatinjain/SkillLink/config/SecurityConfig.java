0ed8e07cf5afc968cfa09678869c41c67ff8163e
JatinJain123 <jatinjain0529@gmail.com>
2025-06-19 20:27:01 +0530

    added swagger / open api

--- src/main/java/com/jatinjain/SkillLink/config/SecurityConfig.java
+++ src/main/java/com/jatinjain/SkillLink/config/SecurityConfig.java
@@ -15,7 +15,11 @@ public class SecurityConfig{
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(AbstractHttpConfigurer::disable);
